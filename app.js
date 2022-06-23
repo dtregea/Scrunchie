1cff53ca97cf87e3e77901daaccb3e4a95cce2d8
dtregea <daniel.tregea@gmail.com>
2022-06-22 20:47:41 -0400

    Condensed request making with axios

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const discord = require("discord.js");
-const https = require("https");
 const mongoose = require("mongoose");
+const axios = require("axios").default;
+
 mongoose.connect(process.env.DATABASE_LOCATION, (err) => {
   if (err) console.log("Failed to connect to the database");
   else console.log("Connected to database");
@@ -32,180 +33,109 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("messageCreate", (msg) => {
-  console.log(msg.content);
+client.on("messageCreate", async (msg) => {
   if (msg.content.startsWith(".cat")) {
-    // TODO specify gif for a gif
-    //encouragingMessage = msg.content.split("$new ")[1] get string after command
-    let buffers = [];
-    let options = {
-      hostname: "api.thecatapi.com",
-      path: "/v1/images/search",
-      headers: { "x-api-key": process.env.CAT_API_KEY },
-    };
-    const req = https.request(options, (res) => {
-      res
-        .on("data", (data) => {
-          // Push data to a buffer to ensure we get full response first
-          buffers.push(data);
-        })
-        .on("end", () => {
-          // Once all data has been received, send the image URL
-          let catData = JSON.parse(Buffer.concat(buffers).toString());
-          msg.channel.send(catData[0].url);
-        });
-    });
-    req.on("error", (error) => {
-      console.error("Error in requesting random cat.");
-      console.log(error);
-    });
-    req.end();
+    // Send a random cat picture
+    const response = await axios.get(
+      "https://api.thecatapi.com/v1/images/search",
+      {
+        headers: {
+          "x-api-key": process.env.CAT_API_KEY,
+        },
+      }
+    );
+    msg.channel.send(response?.data[0]?.url);
   } else if (msg.content.startsWith(".breeds")) {
-    let buffers = [];
-    let options = {
-      hostname: "api.thecatapi.com",
-      path: "/v1/breeds",
-      headers: { "x-api-key": process.env.CAT_API_KEY },
-    };
-    const req = https.request(options, (res) => {
-      res
-        .on("data", (data) => {
-          // Push data to a buffer to ensure we get full response first
-          buffers.push(data);
-        })
-        .on("end", () => {
-          // Once all data has been received, send the image URL
-          let catData = JSON.parse(Buffer.concat(buffers).toString());
-          let listOfBreeds = "Heres a list of searchable cat breeds:\n`";
-          catData.forEach((element) => {
-            listOfBreeds += element.name + ", ";
-          });
-          listOfBreeds += "`";
-          msg.channel.send(listOfBreeds);
-        });
+    const response = await axios.get("https://api.thecatapi.com/v1/breeds", {
+      headers: {
+        "x-api-key": process.env.CAT_API_KEY,
+      },
     });
-    req.on("error", (error) => {
-      console.error("Error in requesting all breeds.");
-      console.log(error);
+    let listOfBreeds = "Heres a list of searchable cat breeds:\n`";
+    response?.data?.forEach((element) => {
+      listOfBreeds += element.name + ", ";
     });
-    req.end();
+    listOfBreeds += "`";
+    msg.channel.send(listOfBreeds);
   } else if (msg.content.startsWith(".breed ")) {
     // Get info about a particular Breed
+
     let command = msg.content.split(" ");
-    // Inform Users about command usage if incorrect
     if (command.length == 1) {
       msg.channel.send('Correct command usage: ".breed [breed name]"');
-    } else {
-      // Get the name of the breed from the command arguments
-      let breedName = "";
-      for (let i = 1; i < command.length; i++) {
-        breedName += command[i];
-        if (i != command.length - 1) breedName += "%20";
-      }
-      let buffers = [];
-      let options = {
-        hostname: "api.thecatapi.com",
-        path: "/v1/breeds/search?q=" + breedName,
-        headers: { "x-api-key": process.env.CAT_API_KEY },
-      };
-      // Make request to Cat API to get information on the breed
-      console.log("Sending request for breed info");
-      const req = https.request(options, (res) => {
-        res
-          .on("data", (data) => {
-            // Push data to a buffer to ensure we get full response first
-            buffers.push(data);
-          })
-          .on("end", () => {
-            console.log("Data for breed info has been received");
-            // Once all data has been received, send the image URL
-            let catData = JSON.parse(Buffer.concat(buffers).toString());
-            if (catData != 0) {
-              // On successful breed found, send another request to the Cat API to get a picture of the breed
-              let catImgURLBuffer = [];
-              const catImgReq = https.request(
-                {
-                  hostname: "api.thecatapi.com",
-                  path:
-                    "https://api.thecatapi.com/v1/images/" +
-                    catData[0].reference_image_id,
-                  headers: { "x-api-key": process.env.CAT_API_KEY },
-                },
-                (catImgRes) => {
-                  console.log("Sending request for cat img url");
-                  catImgRes
-                    .on("data", (catData) => {
-                      // Push data to a buffer to ensure we get full response first
-                      catImgURLBuffer.push(catData);
-                    })
-                    .on("end", () => {
-                      console.log("Cat img url received");
-                      // Image url has been received
-                      let catImgUrl = JSON.parse(
-                        Buffer.concat(catImgURLBuffer).toString()
-                      ).url;
-                      // Generate response to send to users
-                      let response =
-                        "Breed: " +
-                        catData[0].name +
-                        "\nAffection Level: " +
-                        getStars(catData[0].affection_level) +
-                        "\nEnergy Level: " +
-                        getStars(catData[0].energy_level) +
-                        "\nIntelligence: " +
-                        getStars(catData[0].intelligence) +
-                        "\n" +
-                        catData[0].description +
-                        "\n" +
-                        catImgUrl;
-                      msg.channel.send(response);
-                    });
-                }
-              );
-              catImgReq.on("error", (error) => {
-                console.error("Error in searching cat by ID.");
-                console.log(error);
-              });
-              catImgReq.end();
-            } else {
-              msg.channel.send(
-                'Breed name of "' +
-                  breedName.replace(/%20/g, " ") +
-                  '" not found. Try .breeds to find a list of valid breeds.'
-              );
-            }
-          });
-      });
-      req.on("error", (error) => {
-        console.error("Error in requesting all breeds.");
-        console.log(error);
-      });
-      req.end();
+      return;
     }
+
+    let breedName = "";
+    for (let i = 1; i < command.length; i++) {
+      breedName += command[i];
+      if (i != command.length - 1) breedName += "%20";
+    }
+
+    const breedResponse = await axios.get(
+      "https://api.thecatapi.com/v1/breeds/search?q=" + breedName,
+      {
+        headers: {
+          "x-api-key": process.env.CAT_API_KEY,
+        },
+      }
+    );
+
+    if (breedData?.data?.length == 0) {
+      msg.channel.send(
+        'Breed name of "' +
+          breedData.replace(/%20/g, " ") +
+          '" not found. Try .breeds to find a list of valid breeds.'
+      );
+    }
+    let breedData = breedResponse?.data[0];
+    const catImage = await axios.get(
+      "https://api.thecatapi.com/v1/images/" + breedData.reference_image_id,
+      {
+        headers: {
+          "x-api-key": process.env.CAT_API_KEY,
+        },
+      }
+    );
+
+    // Format response to send to users
+    let response =
+      "Breed: " +
+      breedData.name +
+      "\nAffection Level: " +
+      getStars(breedData.affection_level) +
+      "\nEnergy Level: " +
+      getStars(breedData.energy_level) +
+      "\nIntelligence: " +
+      getStars(breedData.intelligence) +
+      "\n" +
+      breedData.description +
+      "\n" +
+      catImage?.data.url;
+    msg.channel.send(response);
   } else if (msg.content.startsWith(".meow")) {
+    // Meow at others, save stats
     let command = msg.content.split(" ");
     if (command.length < 2) {
       // TODO change to separate commands to see other peoples stats
-      Meow.countDocuments({ meowerID: msg.author.id }, (err, numMeow) => {
-        Meow.countDocuments({ meowedID: msg.author.id }, (errn, numMeowed) => {
-          if (!(err || errn))
-            msg.channel.send(
-              `<@!${msg.author.id}> Stats:\nMeow'd at others: ${numMeow}\nMeow'd by others: ${numMeowed}`
-            );
-        });
-      });
+      let numMeows = await Meow.countDocuments({ meowerID: msg.author.id });
+      let numMeowed = await Meow.countDocuments({ meowedID: msg.author.id });
+      msg.channel.send(
+        `<@!${msg.author.id}> Stats:\nMeow'd at others: ${numMeows}\nMeow'd by others: ${numMeowed}`
+      );
     } else {
-      let re = new RegExp("<@!*&*[0-9]+>");
+      let validTagRE = new RegExp("<@!*&*[0-9]+>");
       let meowerID = msg.author.id;
-      let meowedID = command[1].substring(3, 21);
-      if (re.test(command[1]) && meowerID !== meowedID) {
-        let newMeow = new Meow({
+      let meowedID = command[1].substring(2, 20);
+      if (validTagRE.test(command[1]) && meowerID != meowedID) {
+        let newMeow = await new Meow({
           meowerID: meowerID,
           meowedID: meowedID,
-        }).save((err, result) => {
-          if (!err) msg.channel.send(`<@!${meowerID}> just meow'd....`);
-          else console.log(err);
-        });
+        }).save();
+
+        if (newMeow) {
+          msg.channel.send(`<@!${meowerID}> just meow'd....`);
+        }
       }
     }
   }
